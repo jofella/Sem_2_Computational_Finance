abf0960377805e3f959e030be671bc3ceea1d5e0
maris.leibold <maris.leibold@stud.uni-goettingen.de>
2024-05-07 18:16:32 +0200

    Both exercisea completed

--- Assignment_02/Maris_Exercise_02.py
+++ Assignment_02/Maris_Exercise_02.py
@@ -15,6 +15,7 @@ import scipy.stats
 import matplotlib.pyplot as plt
 
 #1a
+#includes borrowed parts from the solutions for sheet 01
 def CRR_stock(S_0, r, sigma, T, M):
     # compute values of u, d and q
     delta_t = T / M
@@ -45,13 +46,13 @@ def CRR_AmEuPur(S_0, r, sigma, T, M, K, EU):
         V[:, m] = np.maximum(K - S[:, m], 0)
     #replace the value 1.2 in V, these are nor real
     V[V == 1.2] = 0
+
+    #define recursion function
     def g(k):
         return math.exp(-r * delta_t) * (q * V[1:k + 1, k] + (1 - q) * V[0:k, k])
 
 
     if EU == 1: #in case of European Option
-        # define recursion function
-
 
         # compute call prices at t_i
         for k in range(M, 0, -1):
@@ -104,3 +105,30 @@ plt.plot(V_0_BS, "b")
 plt.show()
 
 print(CRR_AmEuPur(S_0, r, sigma, T, 500, K, 0))
+
+
+
+###Ex2
+
+def EU_Option_BS_MC(S0, r, sigma, T, K, M, f):
+    #Generate BS EU Call Option price for t=0
+    d_1 = (math.log(S0 / K) + (r + 0.5 * sigma**2) *T) / (sigma * T**0.5)
+    d_2 = d_1 - sigma * T**0.5
+    phi = scipy.stats.norm.cdf(d_1)
+    V0_BS = S0 * phi - K * math.exp(-r * T) * scipy.stats.norm.cdf(d_2)
+    #Setup for MC
+    X = np.random.normal(0, 1, size=M)
+    S_T = S0*np.exp((r - sigma**2/2)*T+sigma*T**0.5*X)
+    #Calculate payoff with provided function
+    V0_MC = math.exp(-r*T)*np.mean(f(S_T, K))
+    #95% confidence interval
+    s_var = np.var(S_T)
+    CI95 = [V0_MC - 1.96 * math.sqrt(s_var/M), V0_MC + 1.96 * math.sqrt(s_var/M)]
+    return V0_BS, V0_MC, CI95
+
+def call_payoff(S_T, K):
+    return np.maximum(0, S_T-K)
+
+
+EU_Option_BS_MC(110, 0.04, 0.2, 1, 100, 10000, call_payoff)
+
