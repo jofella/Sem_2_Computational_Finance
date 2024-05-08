c9ba0a685ab662e687f50ec03c4eb469728d1756
jofella <josef1fella@gmail.com>
2024-05-08 20:03:33 +0200

    JF - 07 // added 95%confidence Intervall

--- Assignment_02/JF/JF_Exerciese_07_JosefFella_ThiHaGiangLe_MarisLeibold.py
+++ Assignment_02/JF/JF_Exerciese_07_JosefFella_ThiHaGiangLe_MarisLeibold.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 # 1.Step: Get MC call price
 
 # Fix random seed
-# np.random.seed(28) # my favorite number
+np.random.seed(28) # my favorite number
 
 # Simulate paths
 S_0 = 100
@@ -39,8 +39,17 @@ def Eu_Option_BS_MC(S_0, r, sigma, T, M, f):
     
    # Calculate discounted average = Option price
    V_0 = math.exp(-r * T) * np.mean(f(S_T))
+   
+   # Calculate 95% CI (skript p.13)
+   
+   # Sample variance
+   sample_variance = math.sqrt(np.var(f(S_T), ddof=1))
+   
+   # 95% CI
+   c1 = V_0 - 1.96 * np.sqrt((sample_variance / M))
+   c2 = V_0 + 1.96 * np.sqrt((sample_variance / M))
 
-   return V_0
+   return V_0, c1, c2
 
 # Test function
 S_0 = 100
@@ -49,9 +58,8 @@ sigma = 0.2
 T = 1
 M = 100
 
-Test = Eu_Option_BS_MC(S_0, r, sigma, T, M, f)
-print(Test)
-
+Test_Variable, c1, c2 = Eu_Option_BS_MC(S_0, r, sigma, T, M, f)
+print(Test_Variable, c1, c2)
 
 
 # 2.Step: Get  BS call price -- From 01
@@ -69,7 +77,6 @@ def BlackScholes_EuCall(t, S_0, r, sigma, T, K):
     return V_0
 
 
-
 # 3.Step: Compare prices
 
 # Set parameter
@@ -80,7 +87,7 @@ sigma = 0.2
 T = 1
 K = 100
 M = 10000
-no_MC_call_prices = 500 # carefully chose this
+no_MC_call_prices = 100 # carefully chose this
 
 # Set up container
 Eu_Option_BS_MC_price = Eu_Option_BS_MC(S_0, r, sigma, T, M, f)
@@ -92,14 +99,15 @@ V_0__BS_EuCall = np.empty(no_MC_call_prices, dtype=float)
 
 # Idea is to simulate many MC prices and compare it to the BS model
 for i in range(0, no_MC_call_prices, 1):
-    V_0__MC_EuCall[i] = Eu_Option_BS_MC(S_0, r, sigma, T, M, f)
+    V_0__MC_EuCall[i], _, _ = Eu_Option_BS_MC(S_0, r, sigma, T, M, f) # Removing the CI
     V_0__BS_EuCall[i] = BlackScholes_EuCall(t, S_0, r, sigma, T, K)
 
 
 # Output terminal message
-print(f"The MC price is: {Eu_Option_BS_MC_price} and the \
+print(f"The MC price is: {Eu_Option_BS_MC_price[0]} with CI form {Eu_Option_BS_MC_price[1]} to \
+{Eu_Option_BS_MC_price[2]} and the \
 BS price is: {BlackScholes_EuCall_price}. \
-The difference is {BlackScholes_EuCall_price - Eu_Option_BS_MC_price}")
+The difference is {BlackScholes_EuCall_price - Eu_Option_BS_MC_price[0]}")
 
 
 # Plot Error for 1000 different simulations
