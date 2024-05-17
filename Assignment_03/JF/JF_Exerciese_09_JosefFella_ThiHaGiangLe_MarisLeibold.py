ac8a6729664e0f8eee1601810ea83acd430acc27
jofella <josef1fella@gmail.com>
2024-05-17 03:13:56 +0200

    JF/First copy of 10

--- Assignment_03/JF/JF_Exerciese_09_JosefFella_ThiHaGiangLe_MarisLeibold.py
+++ Assignment_03/JF/JF_Exerciese_09_JosefFella_ThiHaGiangLe_MarisLeibold.py
@@ -7,24 +7,8 @@ import numpy as np
 import scipy.stats
 import matplotlib.pyplot as plt
 
-
 # Fix random seed
-np.random.seed(28) # my favorite number
-
-
-# Set target function -- here TruncNormal
-def target_function(a, b, mu, sigma, N):
-        # Uniform sample over range a and b
-        U_1 = np.random.uniform(a, b, N)
-        
-        # Calculating parts of f(X) -- ref. Sheet
-        transformed_normal_pdf  = scipy.stats.norm.pdf((U_1 - mu) / sigma)
-        transformed_normal_cdf_b = scipy.stats.norm.cdf((b - mu) / sigma)
-        transformed_normal_cdf_a = scipy.stats.norm.cdf((a - mu) / sigma)
-        
-        g_x = transformed_normal_pdf / (sigma * (transformed_normal_cdf_b - transformed_normal_cdf_a))
-
-        return U_1, g_x
+np.random.seed(28)
 
 
 # Set sample generator
@@ -33,38 +17,43 @@ def Sample_TruncNormal_AR(a, b, mu, sigma, N):
     # 1.Step: Set up container & loop counter
     trunc_normal_sample = np.empty(N, dtype=float)
     i = 0
-    c = 9999
-    max_iter = 10000 # safety mechanism
+    c = 1 #must be calculated?
     
-    while i < N and max_iter > 0:
-        # Get samples (2 uniform + target function)
-        U_1, g_x = target_function(a, b, mu, sigma, 1)
-        U_2 = np.random.uniform(0, 1, 1)
+    while i < N:
+        # Generate iid uniform samples
+        y_sample = np.random.uniform(a, b, 1)
+        u_sample = np.random.uniform(0, 1, 1)
         
-        ratio = g_x / c * U_1
+        # Simulate pdfs: f_y trunc normal pdf and g_y = Uniform pdf 
+        f_y = scipy.stats.norm.pdf((y_sample - mu) / sigma) / (sigma * (scipy.stats.norm.cdf((b - mu)/sigma) - scipy.stats.norm.cdf((a - mu)/sigma)))
+        g_y = scipy.stats.uniform.pdf(y_sample, a, b-a) #why b-a?
         
-        # Accept
-        if U_2 <= ratio:
-            trunc_normal_sample[i] = U_2
+        ratio = f_y / (c * g_y)
+
+        # Accept, else reject (remember its about the pdf not the sample itself)
+        if u_sample <= ratio:
+            trunc_normal_sample[i] = y_sample
             i += 1
-        
-        # Reject - basically doing the same but not adding it -- maybe adding a safety mechanism
-        max_iter -= 1
 
     return trunc_normal_sample
 
 
 # Testing
 a = 0
-b = 1
-mu = 0
+b = 2
+mu = 0.5
 sigma = 1
-N = 10 #sample size
+N = 100000 #sample size
 
 
 Test = Sample_TruncNormal_AR(a, b, mu, sigma, N)
 print(Test)
 
+plt.clf()
+plt.hist(Test, density=True)
+x_range = np.linspace(-1,3,1000)
+plt.plot(x_range, scipy.stats.truncnorm.pdf(x_range, (a-mu)/sigma, (b-mu)/sigma, mu, sigma), "r")
+plt.show()
 
 
 
@@ -86,6 +75,6 @@ plt.clf()
 plt.hist(TruncNormal_sample)
 
 plt.title(f'Histogram of normal distribution with $\mu = {mu}$ and $\sigma = {sigma}$') # from matplotlib doc
-plt.xlabel('bins')
+
 
 plt.show()
--- Assignment_03/JF/JF_Exerciese_10_JosefFella_ThiHaGiangLe_MarisLeibold.py
+++ Assignment_03/JF/JF_Exerciese_10_JosefFella_ThiHaGiangLe_MarisLeibold.py
@@ -9,14 +9,105 @@ import matplotlib.pyplot as plt
 
 
 # Fix random seed
-np.random.seed(28) # my favorite number
+np.random.seed(28)
 
 
-# Build function
+# 1.Part: Set option payout functions
+def eu_call_payoff(x, K):
+    return np.maximum(x - K, 0) # more dynamic with K input function
+
+def quanto_call_payoff(S_T, K):
+    return np.maximum(S_T - K, 0) * S_T
+
+
+# 2.Part: Set "Normal" MC price -- Corrected & adapted version of 07 --
+np.random.seed(28)
+
+def Eu_Option_BS_MC(S_0, r, sigma, T, K, M, f):
+   
+   # 1.Step: Set up data
+   X = np.random.normal(0, 1, M)
+   S_T = np.empty(len(X), dtype = float)
+   Y = np.empty(len(X), dtype = float)
+   
+   # 2.Step: Simulate stock price with BS
+   for i in range(0, M, 1):
+        S_T[i] = S_0 * math.exp((r - 0.5 * np.power(sigma, 2)) * T + sigma * np.sqrt(T) * X[i]) # forgot 0.5
+        Y[i] = f(S_T[i], K) #going thru all stock prices, otherwise constant!!!
+        
+   # 3.Step: Calculate V_hat (estimator) and V_0
+   V_hat = np.mean(Y) # Estimator
+   V_0 = math.exp(-r * T) * V_hat
+   
+   # 4.Step: Calculate 95% CI (skript p.13) -- corrected due to mistake
+   epsilon = 1.96 * math.sqrt(np.var(Y) / M) # ref. solutions
+   
+   c1 = math.exp(-r * T) * (V_hat - epsilon)
+   c2 = math.exp(-r * T) * (V_hat + epsilon)
+
+   return V_0, c1, c2
+
+# Test function
+S_0 = 100
+r = 0.05
+sigma = 0.3
+T = 1
+K = 110
+M = 100
+
+
+V_0, V_hat, c1, c2 = Eu_Option_BS_MC(S_0, r, sigma, T, K, M, quanto_call_payoff) # first forgot K
+print(V_0, V_hat, c1, c2)
+
+
+
+# 3. Part: Apply Variance reduction
+np.random.seed(28)
 
 def BS_EuOption_MC_CV(S_0, r, sigma, T, K, M):
     
+    # 1.Step: Define option price functions (here easier leave inside)
+    def quanto_call_payoff(S_T, K):
+        return np.maximum(S_T - K, 0) * S_T
+    
+    def eu_call_payoff(S_T, K): # As control variate: Close to original payoff (+correlation)
+        return np.maximum(S_T - K, 0)
 
+
+    # 2.Step: Set up data
+    X_normal_sample = np.random.normal(0, 1, M)
+    S_T = np.empty(len(X_normal_sample), dtype = float)
+    
+    X_payoff = np.empty(len(X_normal_sample), dtype = float)
+    Y_payoff = np.empty(len(X_normal_sample), dtype = float)
+    
+    
+    # 3.Step: Simulate prices by BS at T
+    for i in range(0, M, 1):
+        S_T[i] = S_0 * math.exp((r - 0.5 * np.power(sigma, 2)) * T + sigma * np.sqrt(T) * X_normal_sample[i]) # forgot 0.5
+        X_payoff[i] = quanto_call_payoff(S_T[i], K)
+        Y_payoff[i] = eu_call_payoff(S_T[i], K) 
+    
+    
+    # 4.Step: Estimate beta -- np.cov retruns a matrix --> either [0,1] or [1,0]
+    beta_hat = np.cov(X_payoff, Y_payoff)[0,1] / np.var(Y_payoff)    
+
+    # 5.Step: Get E(Y) - Essentially the BS price for our EuCall (we know it 2.10)    
+    E_Y = np.mean(Y_payoff)
+    
+    # 6. Step: Calculate estimates and option price 
+    V_hat_CV_beta = np.mean(X_payoff - Y_payoff * beta_hat + beta_hat * E_Y)      
+    V_0 = math.exp(-r * T) * V_hat_CV_beta
+    
+    
+    # 7. Step: Calculate 95%-CI
+    var_hat_CV = np.var(X_payoff) + 1/M * (np.var(Y_payoff * beta_hat - 2 * np.cov(X_payoff, Y_payoff)[0,1] ))
+    epsilon = 1.96 * math.sqrt(var_hat_CV / M) # ref. solutions
+   
+    c1 = math.exp(-r * T) * (V_hat_CV_beta - epsilon)
+    c2 = math.exp(-r * T) * (V_hat_CV_beta + epsilon)
+
+    return V_0, c1, c2
 
 
 # Test function
@@ -25,35 +116,33 @@ r = 0.05
 sigma = 0.3
 T = 1
 K = 110
-M = 100000
-
-V0 = BS_EuOption_MC_CV(S0, r, sigma, T, K, M)
-    
+M = 10000
 
 
-# MC from 07 --- Copy from solution
-def Eu_Option_BS_MC(S_0, r, sigma, T, K, M, f):
-    # generate M samples
-    X = np.random.normal(0, 1, M)
-    ST = np.empty(len(X), dtype=float)
-    Y = np.empty(len(X), dtype=float)
-
-    # compute ST and Y for each sample
-    for i in range(0, len(X)):
-        ST[i] = S0 * math.exp((r - 0.5 * math.pow(sigma, 2)) * T + sigma * math.sqrt(T) * X[i])
-        Y[i] = f(ST[i], K)
-
-    # calculate V0
-    VN_hat = np.mean(Y)
-    # calculate V0
-    V0 = math.exp(-r * T) * VN_hat
-
-    # compute confidence interval
-    epsilon = 1.96 * math.sqrt(np.var(Y) / M)
-    c1 = math.exp(-r * T) * (VN_hat - epsilon)
-    c2 = math.exp(-r * T) * (VN_hat + epsilon)
-    return V0, VN_hat, c1, c2
+test, c1, c2 = BS_EuOption_MC_CV(S_0, r, sigma, T, K, M)
+print(test, c1, c2)
 
 
 
-# Comparing the confidence intervals (since variance reduction method)
+# 3. Part: Comparing the confidence intervals (since variance reduction method)
+
+# parameters
+S_0 = 100
+r = 0.05
+sigma = 0.3
+T = 1
+K = 110
+M = 10000
+
+
+# Set up output data
+V_0_vanilla_MC, c1_vanilla_MC, c2_vanilla_MC = Eu_Option_BS_MC(S_0, r, sigma, T, K, M, quanto_call_payoff)
+V_0_CV_MC, c1_CV_MC, c2_CV_MC = BS_EuOption_MC_CV(S_0, r, sigma, T, K, M)
+
+
+# Output results
+print(f"For vanilla MC we get V0: {V_0_vanilla_MC} and CI: {c1_vanilla_MC} to {c2_vanilla_MC}")
+print(f"For variance reduction (CV) MC we get V0: {V_0_CV_MC} and CI: {c1_CV_MC} to {c2_CV_MC}")
+print(f"The difference is {c2_CV_MC - c1_CV_MC} to {c2_vanilla_MC - c1_vanilla_MC}")
+
+# Result should be that its tighter, but the price the same ...
