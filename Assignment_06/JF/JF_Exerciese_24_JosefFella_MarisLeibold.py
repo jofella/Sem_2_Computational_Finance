da00f9784db35966d3ebc06421e3e0687c99809d
jofella <josef1fella@gmail.com>
2024-06-11 01:40:35 +0200

    Add correction comment 05

--- Assignment_06/JF/JF_Exerciese_24_JosefFella_MarisLeibold.py
+++ Assignment_06/JF/JF_Exerciese_24_JosefFella_MarisLeibold.py
@@ -6,11 +6,21 @@
 
 import math
 import numpy as np
-import scipy.stats
+import scipy.integrate
 import matplotlib.pyplot as plt
 
 
+
+# Option paypoff
+def g(x):
+    return np.maximum(k-x)
+
+
 def BS_AmPerpPut_ODE(S_max, N, r, sigma, K):
+    
+    
+    np.power(sigma, 2) / 2
+    
     
     return
 
@@ -20,9 +30,21 @@ def BS_AmPerpPut_ODE(S_max, N, r, sigma, K):
 S_max = 200
 N = 200
 r = 0.05
+k = 100
 sigma = np.sqrt(0.4) # sigma^2
 K = 100
 
 
 # Test function
 V0 = BS_AmPerpPut_ODE(S_max, N, r, sigma, K)
+
+
+# Plot data
+plt.clf()
+plt.plot()
+
+plt.title('Change of delta with different strike prices')
+plt.xlabel('strike price (€)')
+plt.ylabel('delta')
+
+plt.show()
